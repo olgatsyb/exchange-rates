13a3c85cca7198b252ac94a66e93d9523469ca8d
Olga <helgatsyb@gmail.com>
2021-08-03 00:13:38 +0300

    first check

--- js/app.js
+++ js/app.js
@@ -1,25 +1,26 @@
 'use strict';
-function updateExchangeRates(update) {
-    const newDate = document.getElementById('date');
-    newDate.textContent = update.date;
-    const newUsdBuyRate = document.getElementById('usd-buy-rate');
-    newUsdBuyRate.textContent = update.usd.buyRate;
-    const newUsdSellRate = document.getElementById('usd-sell-rate');
-    newUsdSellRate.textContent = update.usd.sellRate;
-    const newEurBuyRate = document.getElementById('eur-buy-rate');
-    newEurBuyRate.textContent = update.eur.buyRate;
-    const newEurSellRate = document.getElementById('eur-sell-rate');
-    newEurSellRate.textContent = update.eur.sellRate;
+function handleSubmit() {
+//    evt.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:9999/api/hw13', false);
+    xhr.send();
+    if (xhr.status !== 200) {
+        // обработать ошибку
+        alert(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
+    } else {
+        // вывести результат
+        alert(xhr.responseText); // responseText -- текст ответа.
+    }
+    const data = JSON.parse(xhr.responseText);
+    dateEl.textContent = data.date;
+    usdBuyRateEl.textContent = data.usd.buyRate;
+    usdSellRateEl.textContent = data.usd.sellRate;
+    eurBuyRateEl.textContent = data.eur.buyRate;
+    eurSellRateEl.textContent = data.eur.sellRate;
 }
-const update = {
-    date: '26 июля',
-    usd: {
-        buyRate: '74,75',
-        sellRate: '76,24',
-    },
-    eur: {
-        buyRate: '87,70',
-        sellRate: '89,74',
-    },
-};
-updateExchangeRates(update);
+const dateEl = document.getElementById('date');
+const usdBuyRateEl = document.getElementById('usd-buy-rate');
+const usdSellRateEl = document.getElementById('usd-sell-rate');
+const eurBuyRateEl = document.getElementById('eur-buy-rate');
+const eurSellRateEl = document.getElementById('eur-sell-rate');
+handleSubmit()
